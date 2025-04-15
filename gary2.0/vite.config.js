e65bc9d13fc2f934ddf9f93e1f49d8b3ef480606
apreda <apreda31@gmail.comm>
2025-04-15 15:57:46 -0400

    Fix environment variable handling for real API access: revert fallbacks

--- gary2.0/vite.config.js
+++ gary2.0/vite.config.js
@@ -1,17 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  // Expose process.env as import.meta.env in JS
+  const processEnvValues = {
+    'process.env': Object.entries(env).reduce(
+      (prev, [key, val]) => {
+        return {
+          ...prev,
+          [key]: val,
+        }
+      },
+      {},
+    )
+  }
+
+  return {
+    plugins: [react()],
+    define: processEnvValues,
+    build: {
+      sourcemap: true
     },
-  },
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
+  }
 })
