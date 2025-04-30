584ba6da75a93b56a42fd9f9ee3a7a27fed1a09d
apreda <apreda31@gmail.comm>
2025-04-30 17:52:01 -0400

    Add right navigation arrow for card stack navigation

--- gary2.0/src/pages/RealGaryPicks.jsx
+++ gary2.0/src/pages/RealGaryPicks.jsx
@@ -432,6 +432,18 @@ function RealGaryPicks() {
                         </svg>
                       </button>
                       
+                      {/* Right navigation arrow */}
+                      <button 
+                        className="absolute right-4 z-30 text-white bg-black bg-opacity-50 hover:bg-opacity-70 p-4 rounded-full transition-all duration-300"
+                        onClick={nextPick}
+                        disabled={animating || picks.length <= 1}
+                        style={{ transform: 'translateY(-50%)', top: '50%' }}
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M9 18l6-6-6-6" />
+                        </svg>
+                      </button>
+                      
                       {/* Card counter */}
                       <div className="absolute bottom-4 left-0 right-0 text-center">
                         <span className="px-4 py-2 bg-black bg-opacity-50 rounded-full text-sm text-white">
