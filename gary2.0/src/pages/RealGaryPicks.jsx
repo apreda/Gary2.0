296312cc82c9af389a029e31840a923c3e44fcd2
apreda <apreda31@gmail.comm>
2025-05-08 11:31:40 -0400

    Add more top padding to Gary's Picks page

--- gary2.0/src/pages/RealGaryPicks.jsx
+++ gary2.0/src/pages/RealGaryPicks.jsx
@@ -488,7 +488,7 @@ function RealGaryPicks() {
         }} />
       </div>
       {/* Main content, zIndex: 2 */}
-      <div className="w-full flex flex-col items-center justify-center py-6 px-4 relative" style={{ minHeight: '100vh', zIndex: 2 }}>
+      <div className="w-full flex flex-col items-center justify-center pt-12 pb-6 px-4 relative" style={{ minHeight: '100vh', zIndex: 2 }}>
         <>
           {loading ? (
             <div className="mx-auto max-w-md text-center py-4 px-6 rounded-lg" style={{ backgroundColor: '#121212', border: '3px solid #d4af37' }}>
