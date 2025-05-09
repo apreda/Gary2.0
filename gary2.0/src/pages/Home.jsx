a2b95993404ded0ede476280779b0a5ea9e5b373
apreda <apreda31@gmail.comm>
2025-05-08 23:27:40 -0400

    Reduce spacing between banner and headline by 50%

--- gary2.0/src/pages/Home.jsx
+++ gary2.0/src/pages/Home.jsx
@@ -517,7 +517,7 @@ function Home() {
                 {/* Centered Hero Content */}
              <div className="w-full mx-auto flex flex-col items-center mt-14" style={{ paddingLeft: "0", paddingRight: "0" }}>
               {/* NEW badge with coin image - shifted left */}
-              <div className="relative mt-11 flex justify-center items-center w-full" style={{ marginLeft: "-60px", marginBottom: "1rem" }}>
+              <div className="relative mt-11 flex justify-center items-center w-full" style={{ marginLeft: "-60px", marginBottom: "0.5rem" }}>
                 {/* Coin image - 20% larger than before and moved even further left */}
                 <div className="mr-14">
                   <img
