b50c5f4ad105146ce924024568bcd7bc94fceb38
apreda <apreda31@gmail.comm>
2025-05-08 23:19:48 -0400

    Double coin image size and move it further left

--- gary2.0/src/pages/Home.jsx
+++ gary2.0/src/pages/Home.jsx
@@ -518,14 +518,14 @@ function Home() {
              <div className="w-full mx-auto flex flex-col items-center mt-20 md:mt-24" style={{ paddingLeft: "0", paddingRight: "0" }}>
               {/* NEW badge with coin image */}
               <div className="mb-8 relative mt-16 flex justify-center items-center w-full">
-                {/* Coin image - moved left and 5% smaller */}
-                <div className="mr-5">
+                {/* Coin image - doubled in size and moved further left */}
+                <div className="mr-8">
                   <img
                     src="/coin2.png"
                     alt="Gold Coin"
                     className="object-contain"
                     style={{ 
-                      height: "42.75px", /* 5% smaller than 45px */
+                      height: "85.5px", /* 100% bigger than previous 42.75px */
                       animation: "float 6s ease-in-out infinite",
                       filter: "drop-shadow(0 4px 6px rgba(0, 0, 0, 0.5))",
                     }}
