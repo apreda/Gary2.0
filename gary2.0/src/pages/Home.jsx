0b19bd4ca8fd1c2e7d7eae5982caed27441c9c8a
apreda <apreda31@gmail.comm>
2025-05-08 23:23:20 -0400

    Move both coin image and NEW banner to the left equally

--- gary2.0/src/pages/Home.jsx
+++ gary2.0/src/pages/Home.jsx
@@ -516,8 +516,8 @@ function Home() {
               <main className="hero-inner flex flex-col w-full h-full" style={{ padding: "24px 24px" }}>
                 {/* Centered Hero Content */}
              <div className="w-full mx-auto flex flex-col items-center mt-20 md:mt-24" style={{ paddingLeft: "0", paddingRight: "0" }}>
-              {/* NEW badge with coin image */}
-              <div className="mb-8 relative mt-16 flex justify-center items-center w-full">
+              {/* NEW badge with coin image - shifted left */}
+              <div className="mb-8 relative mt-16 flex justify-center items-center w-full" style={{ marginLeft: "-60px" }}>
                 {/* Coin image - 20% larger than before and moved even further left */}
                 <div className="mr-14">
                   <img
