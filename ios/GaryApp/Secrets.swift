0978ad6e934720b82bf73d47d62605b20673b691
Adam Preda <apreda31@gmail.com>
2025-08-19 11:08:29 -0400

    chore(ios): set Supabase anon key for read-only REST access

--- ios/GaryApp/Secrets.swift
+++ ios/GaryApp/Secrets.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum Secrets {
     static let supabaseURL = URL(string: "https://***REMOVED***.supabase.co")!
-    static let supabaseAnonKey = "REPLACE_WITH_PROD_ANON_KEY"
+    static let supabaseAnonKey = "***REMOVED***"
 }
 
 
