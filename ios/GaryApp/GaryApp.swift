3b4db4d21b3dbf61d43fce7552cf605ad7e1e438
Adam Preda <apreda31@gmail.com>
2025-12-07 22:31:31 -0500

    iOS: Settings image 25% larger + always show onboarding for testing

--- ios/GaryApp/GaryApp.swift
+++ ios/GaryApp/GaryApp.swift
@@ -4,16 +4,18 @@ import SwiftUI
 
 @main
 struct GaryApp: App {
-    @AppStorage("hasEntered") private var hasEntered: Bool = false
+    // TODO: Re-enable hasEntered check for production
+    // @AppStorage("hasEntered") private var hasEntered: Bool = false
     
     var body: some Scene {
         WindowGroup {
             Group {
-                if hasEntered {
-                    ContentView()
-                } else {
+                // Temporarily always show AccessView for testing
+                // if hasEntered {
+                //     ContentView()
+                // } else {
                     AccessView()
-                }
+                // }
             }
             .preferredColorScheme(.dark)
         }
