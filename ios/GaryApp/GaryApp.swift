60af40a1bd2eb42603dcb5e9587cf1c1628bd8cc
Adam Preda <apreda31@gmail.com>
2025-12-10 10:40:17 -0500

    Fix: Remove test code that reset welcome screen on every launch

--- ios/GaryApp/GaryApp.swift
+++ ios/GaryApp/GaryApp.swift
@@ -6,11 +6,6 @@ import SwiftUI
 struct GaryApp: App {
     @AppStorage("hasEntered") private var hasEntered: Bool = false
     
-    init() {
-        // Reset for testing - REMOVE FOR PRODUCTION
-        UserDefaults.standard.set(false, forKey: "hasEntered")
-    }
-    
     var body: some Scene {
         WindowGroup {
             Group {
